73df0292db6d78d5d220ff09648191dfb20d7759
FingerFRK <shayempvp@gmail.com>
2019-05-19 04:06:19 +0200

    Little Update

--- public/index.php
+++ public/index.php
@@ -1,19 +1,15 @@
 <?php
 
     // Views
-
     require_once '../app/View/View.php';
     $views = new View();
 
     // Router
-
     require_once '../app/Router/Router.php';
     $router = new Router();
     require_once '../routes/web.php';
 
-
     // Work
-
     if ($router->match("/".$_GET['url'])) {
         echo $router->call("/".$_GET['url']);
     } else {
